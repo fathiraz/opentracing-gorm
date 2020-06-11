15e98d063f71f964dad4db83cf6db53b8f31a1f6
arthuro <arthurofathiraz@gmail.com>
2020-06-11 16:04:12 +0700

    UPDATE: db dialect, db instance, db param, db show error messages, db statement with values

--- otgorm.go
+++ otgorm.go
@@ -2,7 +2,9 @@ package otgorm
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"github.com/lib/pq"
 	"reflect"
 	"strings"
 	"time"
@@ -89,35 +91,7 @@ func (c *callbacks) after(scope *gorm.Scope, operation string) {
 	}
 
 	// set db full statement tracing tag
-	replacer := make([]string, 0)
-	for i := 1; i <= len(scope.SQLVars); i++ {
-		var sqlValue = ""
-
-		// get value from sql vars
-		val := scope.SQLVars[i-1]
-
-		// get reflect
-		ref := reflect.ValueOf(val).Kind()
-
-		// check for reflect kind of string
-		switch ref {
-		case reflect.String:
-			sqlValue = fmt.Sprintf(`'%s'`, val)
-		case reflect.Interface:
-			if _, ok := val.(time.Time); ok {
-				sqlValue = fmt.Sprintf(`'%v'`, val)
-			}
-		default:
-			sqlValue = fmt.Sprintf(`%v`, val)
-		}
-
-		// push to replacer
-		replacer = append(replacer, fmt.Sprintf(`$%d`, i), sqlValue)
-	}
-
-	// replace statement
-	r := strings.NewReplacer(replacer...)
-	statement := r.Replace(scope.SQL)
+	statement := setStatement(scope)
 	ext.DBStatement.Set(sp, statement)
 
 	sp.Finish()
@@ -145,4 +119,82 @@ func registerCallbacks(db *gorm.DB, name string, c *callbacks) {
 		db.Callback().RowQuery().Before(gormCallbackName).Register(beforeName, c.beforeRowQuery)
 		db.Callback().RowQuery().After(gormCallbackName).Register(afterName, c.afterRowQuery)
 	}
+}
+
+func setStatement(scope *gorm.Scope) string {
+	replacer := make([]string, 0)
+	for i := 1; i <= len(scope.SQLVars); i++ {
+		var sqlValue string
+
+		// get value from sql vars
+		val := scope.SQLVars[i-1]
+
+		// get reflect
+		ref := reflect.ValueOf(val).Kind()
+
+		// check for reflect kind of string
+		switch ref {
+		case reflect.String:
+			sqlValue = fmt.Sprintf(`'%s'`, val)
+		case reflect.Interface:
+
+			// set default value null
+			sqlValue = "NULL"
+
+			// check type of interface
+			switch val.(type) {
+			case time.Time:
+				time := val.(time.Time)
+				sqlValue = fmt.Sprintf(`'%v'`, time.String())
+			case sql.NullTime:
+				null := val.(sql.NullTime)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`'%v'`, null.Time.String())
+				}
+			case sql.NullString:
+				null := val.(sql.NullString)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`'%v'`, null.String)
+				}
+			case sql.NullInt64:
+				null := val.(sql.NullInt64)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`%v`, null.Int64)
+				}
+			case sql.NullInt32:
+				null := val.(sql.NullInt32)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`%v`, null.Int32)
+				}
+			case sql.NullBool:
+				null := val.(sql.NullBool)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`%v`, null.Bool)
+				}
+			case sql.NullFloat64:
+				null := val.(sql.NullFloat64)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`%v`, null.Float64)
+				}
+			case pq.NullTime:
+				null := val.(pq.NullTime)
+				if null.Valid {
+					sqlValue = fmt.Sprintf(`'%v'`, null.Time)
+				}
+			}
+		default:
+			sqlValue = fmt.Sprintf(`%v`, val)
+		}
+
+		// push to replacer
+		replacer = append(replacer, fmt.Sprintf(`$%d`, i), sqlValue)
+	}
+
+	// replace statement
+	r := strings.NewReplacer(replacer...)
+
+	// set result
+	result := r.Replace(scope.SQL)
+
+	return result
 }
